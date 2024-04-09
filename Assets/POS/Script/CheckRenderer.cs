3b2344960445fc2b749ca157b902c25ddf73241d
AyoAyomide <ayoayomide124@gmail.com>
2024-04-09 14:18:00 +0100

    v2

---
+++ Assets/POS/Script/CheckRenderer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CheckRenderer : MonoBehaviour
+{
+    [SerializeField] Renderer meshRenderer;
+
+    private void Update(){
+        // if(meshRenderer.isVisible){
+        //     Debug.Log("Object can be seen");
+        // }
+    }
+}
--- Assets/POS/Script/Xframe.cs
+++ Assets/POS/Script/Xframe.cs
@@ -1,28 +1,49 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Xframe : MonoBehaviour
 {
     private List<float> myData = new List<float>();
 
-    private int interval = 24;
+    private float timeSinceLastCalled;
+
+    [SerializeField] TextMeshProUGUI debugMessage;
+
     private void Update()
     {
-        Debug.Log("A frame");
-        // if (Time.frameCount % interval == 0) SomeFunction();
-        RunOn(10, SomeFunction);
-        RunOn(24, SomeFunction);
+
+        debugMessage.text = "FPS: "+Time.frameCount;
+        RunOn(60, SomeFunction);
+        RunPer(2, SomeFunction2);
     }
 
     private void SomeFunction()
     {
-        Debug.Log("3rd frame");
+        Debug.Log("Run on 3rd");
         myData.Add(0);
     }
 
-    private void RunOn(int xframe,Action funToRun){
+    private void SomeFunction2()
+    {
+        Debug.Log("Run per 1 frame");
+        myData.Add(0);
+    }
+
+    private void RunOn(int xframe, Action funToRun)
+    {
         if (Time.frameCount % xframe == 0) funToRun();
+    }
+
+    private void RunPer(int delay, Action funToRun)
+    {
+        timeSinceLastCalled += Time.deltaTime;
+        if (timeSinceLastCalled > delay)
+        {
+            funToRun();
+            timeSinceLastCalled = 0f;
+        }
     }
 }
